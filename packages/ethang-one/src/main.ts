3f34088780993445abeceb75ef8877fe10e48194
Ethan Glover <hello@ethang.email>
2021-12-31 17:02:00 -0600

    dep manage fix

--- packages/ethang-one/src/main.ts
+++ packages/ethang-one/src/main.ts
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import inquirer, { Separator } from 'inquirer';
+import inquirer, {Separator} from 'inquirer';
 
-import { nodeDependencies } from './app/node-dependencies/node-dependencies';
-import { buildOrRunProjects } from './app/project-management/build-or-run-projects';
-import { projectManagement } from './app/project-management/project-management';
-import { projectsManagement } from './app/projects-management/projects-management';
+import {nodeDependencies} from './app/node-dependencies/node-dependencies';
+import {buildOrRunProjects} from './app/project-management/build-or-run-projects';
+import {projectManagement} from './app/project-management/project-management';
+import {projectsManagement} from './app/projects-management/projects-management';
 
-export const packageManager = 'corepack pnpm';
+export const packageManager = 'npx pnpm';
 export const packageManagerInstallAll = `${packageManager} i`;
 export const packageManagerAdd = packageManagerInstallAll;
 export const packageManagerRemove = `${packageManager} remove`;
@@ -15,20 +15,20 @@ enum Option {
   exit = 'Exit',
   buildOrRunProjectCommand = 'Build or Run Project(s)',
   projectManagementCommand = 'Project Generate (New Component, New Page, etc.)',
-  nodeDependenciesCommand = `Manage NPM dependencies.`,
+  nodeDependenciesCommand = 'Manage NPM dependencies.',
   projectsManagementCommand = 'Manage Project List (Create New, Remove)',
 }
 
 async function runEthanGOne(): Promise<void> {
-  const { option } = await inquirer.prompt<{ option: Option }>([
+  const {option} = await inquirer.prompt<{option: Option}>([
     {
       choices: [
-        { name: Option.buildOrRunProjectCommand },
-        { name: Option.projectManagementCommand },
-        { name: Option.nodeDependenciesCommand },
-        { name: Option.projectsManagementCommand },
+        {name: Option.buildOrRunProjectCommand},
+        {name: Option.projectManagementCommand},
+        {name: Option.nodeDependenciesCommand},
+        {name: Option.projectsManagementCommand},
         new Separator(),
-        { name: Option.exit },
+        {name: Option.exit},
       ],
       message: 'Choose an option:',
       name: 'option',
