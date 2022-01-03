32e6fbd22d20a421615da29b4344993b807f2dd6
Ethan Glover <hello@ethang.email>
2022-01-02 18:45:45 -0600

    this wont work

--- packages/website/pages/youtube-subscriptions/index.tsx
+++
@@ -1,5 +0,0 @@
-import {YoutubeSubscriptionsLayout} from '../../components/youtube-subscriptions-layout/youtube-subscriptions-layout';
-
-const YoutubeSubscriptions = (): JSX.Element => <YoutubeSubscriptionsLayout/>;
-
-export default YoutubeSubscriptions;
