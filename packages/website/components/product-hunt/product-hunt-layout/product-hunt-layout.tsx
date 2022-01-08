90cede09f945496c6485e9623c7307c7baf8a118
Ethan Glover <hello@ethang.email>
2022-01-08 16:31:17 -0600

    woops

--- packages/website/components/product-hunt/product-hunt-layout/product-hunt-layout.tsx
+++ packages/website/components/product-hunt/product-hunt-layout/product-hunt-layout.tsx
@@ -1,24 +1,28 @@
 import getYouTubeID from 'get-youtube-id';
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
-import {Container} from '../../common/container/container';
-import {HeadTag} from '../../common/head-tag/head-tag';
-import {LinkComponent} from '../../common/link-component/link-component';
-import {LoadingImage} from '../../common/loading-image/loading-image';
-import {YouTubeEmbed} from '../../common/youtube-embed/youtube-embed';
+import { Container } from '../../common/container/container';
+import { HeadTag } from '../../common/head-tag/head-tag';
+import { LinkComponent } from '../../common/link-component/link-component';
+import { LoadingImage } from '../../common/loading-image/loading-image';
+import { YouTubeEmbed } from '../../common/youtube-embed/youtube-embed';
 import styles from './product-hunt-layout.module.css';
-import {ProductHuntData} from './product-hunt-layout-types';
+import { ProductHuntData } from './product-hunt-layout-types';
 
 export const ProductHuntLayout = (): JSX.Element => {
   const [data, setData] = useState<ProductHuntData>();
 
   useEffect(() => {
     const headers = new Headers();
-    headers.append('Authorization', 'Bearer IWDedkSZEl-pjNJv8Rg_eQUM2ACQ_RG7-anzER-igGY');
+    headers.append(
+      'Authorization',
+      `Bearer ${process.env.NX_PRODUCT_HUNT_ACESS_TOKEN ?? ''}`
+    );
     headers.append('Content-Type', 'application/json');
 
     const graphql = JSON.stringify({
-      query: '{ posts(order: NEWEST) { edges { node { id name description media { url videoUrl } productLinks { type url }}}}}',
+      query:
+        '{ posts(order: NEWEST) { edges { node { id name description media { url videoUrl } productLinks { type url }}}}}',
       variables: {},
     });
     const requestOptions = {
@@ -40,31 +44,47 @@ export const ProductHuntLayout = (): JSX.Element => {
   }, []);
 
   if (typeof data !== 'undefined') {
-    <LoadingImage/>;
+    <LoadingImage />;
   }
 
   if (data) {
     return (
       <Container>
-        <HeadTag title="Product Hunt"/>
+        <HeadTag title="Product Hunt" />
         <div className={styles.ProductHuntContainer as string}>
           {data.posts.edges.map(post => {
             return (
               <div key={post.node.id}>
-                <div className={styles.ProductHuntTitle as string}>{post.node.name}</div>
+                <div className={styles.ProductHuntTitle as string}>
+                  {post.node.name}
+                </div>
                 <div>{post.node.description}</div>
                 <div className={styles.Links as string}>
                   {post.node.productLinks.map(productLink => {
                     return (
-                      <div key={productLink.url}><LinkComponent
-                        content={productLink.type} href={productLink.url}/>&emsp;
+                      <div key={productLink.url}>
+                        <LinkComponent
+                          content={productLink.type}
+                          href={productLink.url}
+                        />
+                        &emsp;
                       </div>
                     );
                   })}
                 </div>
-                <div>{typeof post.node.media[0].videoUrl === 'string'
-                  ? <YouTubeEmbed id={getYouTubeID(post.node.media[0].videoUrl)} title={post.node.name}/>
-                  : <img style={{paddingTop: '1rem'}} src={post.node.media[0].url} alt={post.node.name}/>}
+                <div>
+                  {typeof post.node.media[0].videoUrl === 'string' ? (
+                    <YouTubeEmbed
+                      id={getYouTubeID(post.node.media[0].videoUrl)}
+                      title={post.node.name}
+                    />
+                  ) : (
+                    <img
+                      style={{ paddingTop: '1rem' }}
+                      src={post.node.media[0].url}
+                      alt={post.node.name}
+                    />
+                  )}
                 </div>
               </div>
             );
