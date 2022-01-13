49a622aa0ec9f483388343b924e3d012f3e6a2d4
Ethan Glover <hello@ethang.email>
2022-01-12 18:02:25 -0600

    Automatic Update: 2022-01-13T00:02:24.162Z

--- packages/computer-cli/src/app/constants.ts
+++ packages/computer-cli/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const PROJECT_ROOT = '~/Projects/ethang-one/packages/computer-server';
+export const PROJECT_ROOT = '~/Projects/ethang-one/packages/computer-cli';
 export const SAVED_FILES_ROOT = `${PROJECT_ROOT}/src/app/commands/saved-files`;
 
 export enum Shell {
