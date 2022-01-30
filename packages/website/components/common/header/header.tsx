6fbe8dd3ad37ef0afc2f3f93f73ac01426a0f623
Ethan Glover <hello@ethang.email>
2022-01-30 08:18:26 -0600

    version bump remove gql dep from website

--- packages/website/components/common/header/header.tsx
+++ packages/website/components/common/header/header.tsx
@@ -1,39 +1,18 @@
-import {useReactiveVar} from '@apollo/client';
-
-import {
-  isUserAdminVariable,
-  userIdVariable,
-} from '../../../graphql/reactive-variables';
-import {LinkComponent} from '../link-component/link-component';
+import { LinkComponent } from '../link-component/link-component';
 import styles from './header.module.css';
-import {Navigation} from './navigation';
+import { Navigation } from './navigation';
 
 export const Header = (): JSX.Element => {
-  const isUserAdmin = useReactiveVar(isUserAdminVariable);
-  const userId = useReactiveVar(userIdVariable);
-
-  const handleLogOut = (): void => {
-    userIdVariable(null);
-    isUserAdminVariable(false);
-  };
-
-  const headerContent = (
-    <h1 className={styles.Header}>{isUserAdmin ? 'Admin' : 'EthanG'}</h1>
-  );
+  const headerContent = <h1 className={styles.Header as string}>EthanG</h1>;
 
   return (
     <header>
       <LinkComponent
-        styles={{textDecoration: 'none'}}
+        styles={{ textDecoration: 'none' }}
         content={headerContent}
-        href={isUserAdmin ? '/admin' : '/'}
+        href="/"
       />
-      {isUserAdmin && userId && (
-        <button type="button" onClick={handleLogOut}>
-          Log Out
-        </button>
-      )}
-      <Navigation/>
+      <Navigation />
     </header>
   );
 };
