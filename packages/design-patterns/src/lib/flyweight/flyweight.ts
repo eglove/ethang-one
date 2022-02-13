62fa92eb54d7d114fd863c232fda7ce9cf00caf8
Ethan Glover <hello@ethang.email>
2022-02-13 08:59:31 -0600

    Automatic Update: 2022-02-13T14:59:28.639Z

---
+++ packages/design-patterns/src/lib/flyweight/flyweight.ts
@@ -0,0 +1,53 @@
+export class Book {
+  public author: string;
+  public isbn: string;
+  public title: string;
+
+  constructor(author: string, isbn: string, title: string) {
+    this.author = author;
+    this.isbn = isbn;
+    this.title = title;
+  }
+}
+
+const books: Map<string, Book> = new Map();
+const bookList: Book[] = [];
+
+const createBook = (author: string, isbn: string, title: string): Book => {
+  const existingBook = books.has(isbn);
+
+  if (existingBook) {
+    return books.get(isbn)!;
+  }
+
+  // Reuse constructor to avoid creating new object all the time
+  const book = new Book(author, isbn, title);
+  books.set(isbn, book);
+
+  return book;
+};
+
+const addBook = (
+  author: string,
+  isbn: string,
+  title: string,
+  availability: boolean,
+  sales: number
+): Book => {
+  const book = {
+    ...createBook(author, isbn, title),
+    availability,
+    isbn,
+    sales,
+  };
+
+  bookList.push(book);
+  return book;
+};
+
+addBook('JK Rowling', 'AB123', 'Harry Potter', false, 100);
+addBook('JK Rowling', 'AB123', 'Harry Potter', true, 50);
+addBook('Harper Lee', 'CD345', 'To Kill a Mockingbird', true, 10);
+addBook('Harper Lee', 'CD345', 'To Kill a Mockingbird', false, 20);
+addBook('F. Scott Fitzgerald', 'EF567', 'The Great Gatsby', false, 20);
+console.log(bookList);
---
+++ packages/design-patterns/src/lib/mediator/mediator.ts
@@ -0,0 +1,35 @@
+export class ChatRoom {
+  logMessage(user: User, message: string): void {
+    const time = new Date();
+    const sender = user.getName();
+
+    console.log(`${time.toISOString()} [${sender}]: ${message}`);
+  }
+}
+
+export class User {
+  public chatroom: ChatRoom;
+  public name: string;
+
+  constructor(name: string, chatroom: ChatRoom) {
+    this.name = name;
+    this.chatroom = chatroom;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  send(message: string): void {
+    this.chatroom.logMessage(this, message);
+  }
+}
+
+const chatRoom = new ChatRoom();
+
+const user1 = new User('John Doe', chatRoom);
+const user2 = new User('Jane Doe', chatRoom);
+
+// Users don't send messages to one another, messages are sent to the chatroom (the mediator)
+user1.send('hi');
+user2.send('hello');
