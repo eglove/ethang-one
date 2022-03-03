608543fbf1bcbd5a6c06c1828ffc69fb956791d9
Ethan Glover <hello@ethang.email>
2022-03-02 19:02:00 -0600

    blogs complete

--- packages/website/database/data/images.ts
+++ packages/website/database/data/images.ts
@@ -3,14 +3,20 @@ import { blogImage, Image, technologyLogoImage } from '../models/image';
 export enum ImageName {
   anki = 'anki',
   bitcoin = 'bitcoin',
+  completeJuniorSenior = 'completeJuniorSenior',
+  completeWebDeveloper = 'completeWebDeveloper',
   ethanAvatar = 'ethanAvatar',
   expectationsVsReality = 'expectationsVsReality',
+  gitBootcamp = 'gitBootcamp',
   laconiaBikeWeek = 'laconiaBikeWeek',
+  learningToLearn = 'learningToLearn',
   microphone = 'microphone',
+  missingKeys = 'missingKeys',
   moralFoundationsTheory = 'moralFoundationsTheory',
   paradox = 'paradox',
   politicsAsAVocation = 'politicsAsAVocation',
   portfolioRedesign = 'portfolioRedesign',
+  postmanCompleteGuide = 'postmanCompleteGuide',
   powershellScript = 'powershellScript',
   openLetterLibertarians = 'openLetterLibertarians',
   react = 'react',
@@ -22,6 +28,7 @@ export enum ImageName {
   tinyMce = 'tinyMce',
   typescript = 'typescript',
   wantToBelieve = 'wantToBelieve',
+  webDeveloperBootcamp = 'webDeveloperBootcamp',
 }
 
 type ImageMetaData = Record<ImageName, Image>;
@@ -39,6 +46,18 @@ export const images: ImageMetaData = {
     url: technologyLogoImage('bitcoin.svg'),
     width: 64,
   }),
+  [ImageName.completeJuniorSenior]: new Image({
+    altText: 'Complete Junior to Senior Web Developer',
+    height: 397,
+    url: blogImage('complete-junior-senior.png'),
+    width: 705,
+  }),
+  [ImageName.completeWebDeveloper]: new Image({
+    altText: 'Desktop computer, laptop and mobiel phone',
+    height: 396,
+    url: blogImage('complete-web-developer.png'),
+    width: 705,
+  }),
   [ImageName.ethanAvatar]: new Image({
     altText: 'Ethan Glover Avatar',
     height: 449,
@@ -51,17 +70,35 @@ export const images: ImageMetaData = {
     url: blogImage('expectations-reality.jpg'),
     width: 1000,
   }),
+  [ImageName.gitBootcamp]: new Image({
+    altText: "Colt Steele's Git and GitHub Bootcamp",
+    height: 270,
+    url: blogImage('git-github-bootcamp.jpg'),
+    width: 480,
+  }),
   [ImageName.laconiaBikeWeek]: new Image({
     altText: 'National Guard responds to Laconia Riots',
     height: 300,
     url: blogImage('laconia-bike-week.jpg'),
     width: 242,
   }),
+  [ImageName.learningToLearn]: new Image({
+    altText: 'Mechanical brains',
+    height: 398,
+    url: blogImage('learning-to-learn.png'),
+    width: 705,
+  }),
   [ImageName.microphone]: new Image({
     altText: 'Microphone on stage',
     height: 427,
     url: blogImage('microphone.jpeg'),
     width: 640,
+  }),
+  [ImageName.missingKeys]: new Image({
+    altText: 'Pring out missing environment variable keys.',
+    height: 132,
+    url: blogImage('missing-keys.png'),
+    width: 561,
   }),
   [ImageName.moralFoundationsTheory]: new Image({
     altText: '3 Political Camps',
@@ -87,6 +124,12 @@ export const images: ImageMetaData = {
     height: 743,
     url: blogImage('portfolio-redesign.png'),
     width: 992,
+  }),
+  [ImageName.postmanCompleteGuide]: new Image({
+    altText: 'Valentin Despa giving a talk at Post/Con',
+    height: 270,
+    url: blogImage('postman-complete-guide.jpg'),
+    width: 480,
   }),
   [ImageName.powershellScript]: new Image({
     altText: 'PowerShell script to download and run another script.',
@@ -153,5 +196,11 @@ export const images: ImageMetaData = {
     height: 300,
     url: blogImage('i-want-to-believe.png'),
     width: 240,
+  }),
+  [ImageName.webDeveloperBootcamp]: new Image({
+    altText: 'Colt Steele Web Developer Bootcamp',
+    height: 270,
+    url: blogImage('web-developer-bootcamp.jpg'),
+    width: 480,
   }),
 };
---
+++ packages/website/pages/blog/review-learning-to-learn.tsx
@@ -0,0 +1,49 @@
+import { StaticBlogLayout } from '../../components/blog/blog/static-blog-layout';
+import { blogSlug } from '../../database/data/blogs';
+
+const ReviewLearningToLearn = (): JSX.Element => {
+  return (
+    <StaticBlogLayout slug={blogSlug.reviewLearningToLearn}>
+      <p>
+        Learning to Learn is the perfect first course. Most people wouldn&apos;t
+        give something with that title much attention, but getting started with
+        learning code without taking some time to figure out how you&apos;re
+        going handle it is a big mistake in my opinion. Learning to Learn covers
+        topics like the Feynman Technique, chunking, trunking, the Method of
+        Loci, framing, focus vs. diffuse states, and much more. Knowing about
+        and using these methods is the perfect way to make sure you&apos;re
+        learning as efficiently as possible.
+      </p>
+      <p>
+        The instructor Andrei Neagoie is one of the best course presenters
+        I&apos;ve come across. His pacing, voice, and passion are all on a
+        perfect level. He doesn&apos;t drone on in a monotone voice, he
+        doesn&apos;t sound like he wishes he weren&apos;t there and he can keep
+        you engaged the whole way through.
+      </p>
+      <p>
+        This course brings together information from a lot of sources in a
+        smooth and coherent manner that paints a clear picture and gives the
+        student the ability to build their own path forward. That is not an easy
+        thing to pull off. Andrei smartly covers and dismisses fallacy-like
+        learning techniques such as the 10,000 hours rule and brings together
+        the ones that you can implement and see the effects of today.
+      </p>
+      <p>
+        There&apos;s no other course like this on this level. Finding something
+        similar will get you uninspired drivel of people reading off Wikipedia
+        and bragging about how smart they are. (And to be fair, Andrei does a
+        lot of bragging about how smart he is in this course.){' '}
+      </p>
+      <p>
+        I highly recommend this course for everyone, whether or not you like the
+        idea of &ldquo;learning to learn&rdquo;, or no matter the subject
+        you&apos;re aiming to learn, you will get something positive out of
+        this.
+      </p>
+      <p>5/5</p>
+    </StaticBlogLayout>
+  );
+};
+
+export default ReviewLearningToLearn;
