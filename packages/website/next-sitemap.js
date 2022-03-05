5ae9e3151378a6476ae64c6164b8f61ee6d794e9
Ethan Glover <hello@ethang.email>
2022-03-05 16:26:22 -0600

    sitemap

---
+++ packages/website/next-sitemap.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  generateRobotsTxt: true,
+  outDir: 'dist/packages/website/public/',
+  siteUrl: 'https://www.ethang.dev/',
+  sourceDir: 'dist/packages/website/.next',
+};
