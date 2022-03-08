ecee86d28bcf59ec2f05cf470aabb0609a2f58bd
Ethan Glover <hello@ethang.email>
2022-03-07 19:55:43 -0600

    add search

--- apps/website/components/common/header/navigation.tsx
+++ apps/website/components/common/header/navigation.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import { LinkComponent } from '../link-component/link-component';
 import styles from './header.module.css';
 
@@ -14,21 +16,27 @@ export const Navigation = (): JSX.Element => {
   ];
 
   return (
-    <nav className={styles.Navigation}>
-      {navLinks.map(navLink => {
-        return (
-          <LinkComponent
-            key={navLink.title}
-            linkProperties={{
-              className: styles.NavigationLink,
-              href: navLink.link,
-              key: navLink.title,
-            }}
-          >
-            {navLink.title}
-          </LinkComponent>
-        );
-      })}
-    </nav>
+    <>
+      <nav className={styles.Navigation}>
+        {navLinks.map(navLink => {
+          return (
+            <LinkComponent
+              key={navLink.title}
+              linkProperties={{
+                className: styles.NavigationLink,
+                href: navLink.link,
+                key: navLink.title,
+              }}
+            >
+              {navLink.title}
+            </LinkComponent>
+          );
+        })}
+      </nav>
+      <div className={styles.CseSearch}>
+        <Script src="https://cse.google.com/cse.js?cx=1040596e09bea9882" />
+        <div className="gcse-search" />
+      </div>
+    </>
   );
 };
