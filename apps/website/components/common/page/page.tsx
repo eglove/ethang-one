5ecb69e8807f75a3b28ed0ac300e221776e0d6c5
Ethan Glover <hello@ethang.email>
2022-03-07 19:22:01 -0600

    move analytics

--- apps/website/components/common/page/page.tsx
+++ apps/website/components/common/page/page.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import { PropertiesChildren } from '../../../types/common';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
@@ -7,6 +9,19 @@ import styles from '../styles/particles.module.css';
 export const Page = ({ children }: PropertiesChildren): JSX.Element => {
   return (
     <div className={styles.ParticleContainer}>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZM8N2J2F2Q"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-ZM8N2J2F2Q"');
+        `}
+      </Script>
       <ParticlesContainer />
       <div className={styles.ContentContainer}>
         <Header />
