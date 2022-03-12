d378fb0ac1eedd56c2b1d274f8e319d247daff61
Ethan Glover <hello@ethang.email>
2022-03-12 12:48:13 -0600

    dashboard stuff

--- apps/ethang/node/project-runner.ts
+++ apps/ethang/node/project-runner.ts
@@ -9,6 +9,13 @@ import {
   WebsocketKey,
 } from '../lib/commands';
 
+export const emitTerminal = (socket: Socket, message: string): void => {
+  socket.emit('terminal', {
+    id: uuid(),
+    message,
+  });
+};
+
 export const onScript = (socket: Socket): void => {
   socket.on('script', (message: WebsocketKey) => {
     if (message === WebsocketKey.padRepo) {
@@ -16,52 +23,35 @@ export const onScript = (socket: Socket): void => {
         execSyncCommand('git status -s').toString('utf8').length === 0;
 
       if (isGitStatusClean) {
-        socket.emit('terminal', {
-          id: uuid(),
-          message: 'Git status is clean, adding message.',
-        });
+        emitTerminal(socket, 'Git status is clean, adding message.');
         execSyncCommand(
           `Add-Content -Path ${ETHANG_CLI_ROOT}/src/assets/the-graph.txt "The GitHub contribution graph is a lie."`
         );
       }
 
       const commitMessage = `Automatic Update: ${new Date().toISOString()}`;
-      socket.emit('terminal', {
-        id: uuid(),
-        message: 'Pushing to Git.',
-      });
+      emitTerminal(socket, 'Pushing to Git.');
       runCommand(['git add .', `git commit -m "${commitMessage}"`, 'git push']);
     }
   });
 };
 
 export const onTerminal = (socket: Socket): void => {
-  const emitData = (data: unknown): void => {
-    socket.emit('terminal', {
-      id: uuid(),
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      message: data.toString() as string,
-    });
-  };
-
   socket.on('terminal', (command: string) => {
     const process = runPowershellCommand(
       command.replaceAll(commandSplitter, ' && ')
     );
 
     process.stdout.on('data', data => {
-      emitData(data);
+      emitTerminal(socket, data);
     });
 
     process.stderr.on('data', data => {
-      emitData(data);
+      emitTerminal(socket, data);
     });
 
     process.on('exit', () => {
-      socket.emit('terminal', {
-        id: uuid(),
-        message: 'Exiting.',
-      });
+      emitTerminal(socket, 'Exiting.');
       process.kill();
     });
   });
