c04fa91e41ee2be74e47173424ff4b4bea571efa
Ethan Glover <hello@ethang.email>
2022-03-31 17:01:41 -0500

    fixes

--- apps/website/pages/blog/recent-course-updates.tsx
+++ apps/website/pages/blog/recent-course-updates.tsx
@@ -14,7 +14,8 @@ function RecentCourseUpdates({ blog }: BlogProperties): JSX.Element {
           recommended courses
         </NextLink>{' '}
         list will be listed here. Because it can take two or more years to go
-        through this course, if you want to commit to it, you can{' '}
+        through this course list, if you want to commit to the current version,
+        you can{' '}
         <NextLink
           linkProperties={{ href: '/documents/Development_Curriculum.pdf' }}
         >
@@ -27,7 +28,7 @@ function RecentCourseUpdates({ blog }: BlogProperties): JSX.Element {
       </p>
       <h2>Courses Added</h2>
       <p>
-        <strong>CompTIA A+ Core 1 & 2</strong>: This is a course built for
+        <strong>CompTIA A+ Core 1 & 2</strong>: These two courses are built for
         people wanting to get into IT technical support, it&apos;s not meant for
         developers. But I also believe the content covered is very important.
         You should be familiar with various operating systems, virtual machines,
@@ -37,20 +38,20 @@ function RecentCourseUpdates({ blog }: BlogProperties): JSX.Element {
       </p>
       <p>
         <strong>SEO Training Masterclass</strong>: SEO isn&apos;t covered very
-        often, and when it is, not very well covered. When you get into Next.js
-        you will find very lackluster explanations of &ldquo;SEO&rdquo; that
-        center around static generation and title tags. SEO is more than that.
-        And every web dev should have an understanding of schema markup, link
-        building and site structure. This course will cover all of those and
-        more in a great detail.
+        often, and when it is, it&apos;s not very well covered. When you get
+        into Next.js you will find very lackluster explanations of
+        &ldquo;SEO&rdquo; that center around static generation and title tags.
+        SEO is more than that. And every web dev should have an understanding of
+        schema markup, link building and site structure. This course will cover
+        all of those and more in a great detail.
       </p>
       <p>
         <strong>SCRUM for Beginners / Agile & SCRUM for Product Owners</strong>:
         As you get into your first development positions you&apos;ll pick up the
         general idea of what Agile and SCRUM are. Every company has a different
-        interpretation of it. But learning these things officially from the
-        Agile Manifesto can really help you make better decisions if you find
-        yourself in a SCRUM master or product owner role.
+        interpretation of what it is. But learning these things officially from
+        the Agile Manifesto can really help you make better decisions if you
+        find yourself in a SCRUM master or product owner role.
       </p>
       <h2>Courses Removed</h2>
       <p>
@@ -60,9 +61,10 @@ function RecentCourseUpdates({ blog }: BlogProperties): JSX.Element {
         course and get into better performance analysis in Epic React. With
         CI/CD you&apos;ll pick up how to get started with GitHub automation and
         I&apos;m currently looking at a new Terraform course. As this list
-        grows, this course has simply been replaced. Each particular topic is
-        covered the basics on, you will either find entire coruses, or more in
-        depth explorations of in other courses here.
+        grows, this course has simply been replaced. For each particular topic
+        that&apos;s covered in Junior to Senior, you will either find entire
+        courses, or more in depth explorations of that topic in other courses
+        here.
       </p>
       <p>
         <strong>Complete Web & Mobile Designer</strong>: Similar to Complete
@@ -73,7 +75,7 @@ function RecentCourseUpdates({ blog }: BlogProperties): JSX.Element {
         really doesn&apos;t provide a lot of overlap with what&apos;s here.
         It&apos;s just out of place here. If I can find something on advanced
         CSS, maybe on building HTML5 games I think that would be a more
-        appropriate subject for this course list.
+        appropriate subject for this list.
       </p>
     </StaticBlogLayout>
   );
