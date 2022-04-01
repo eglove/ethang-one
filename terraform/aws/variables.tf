b1f220dabe710016833a9cdae3566be41ec5af58
Ethan Glover <hello@ethang.email>
2022-03-31 19:57:00 -0500

    terraform start

---
+++ terraform/aws/variables.tf
@@ -0,0 +1,13 @@
+variable "default_cidr_block" {}
+variable "default_route_table_name" {}
+variable "main_vpc_name" {}
+variable "subnet_zone" {}
+variable "vpc_cidr_block" {}
+variable "web_subnet" {}
+variable "web_subnet_name" {}
+
+variable "AWS_DEFAULT_REGION" {}
+variable "AWS_ETHANG_ADMIN_ACCESS_KEY" {}
+variable "AWS_ETHANG_ADMIN_SECRET_KEY" {}
+
+
