7cb9df1ecb6c7953d55409fe73a916a31a981dea
ethang <hello@ethang.email>
2022-04-21 20:16:53 -0500

    stick with next component

--- apps/website/db/data/courses/courses.ts
+++ apps/website/db/data/courses/courses.ts
@@ -1,7 +1,27 @@
 import { Course } from '../../models/course';
+import { persons } from '../persons/persons';
 import { schools } from '../schools/schools';
 
 export const courses = {
+  accessibilityForEveryone: new Course({
+    courseUrls: [
+      {
+        school: schools.levelUpTutorials,
+        url: new URL(
+          'https://leveluptutorials.com/tutorials/accessibility-for-everyone/'
+        ),
+      },
+    ],
+    duration: 2.4623,
+    instructors: [persons.amyKapernick],
+    rating: 5,
+    ratingUrl: new URL(
+      'https://www.ethang.dev/blog/review-accessibility-for-everyone'
+    ),
+    recommendationOrder: 4,
+    title: 'Accessibility For Everyone',
+    yearUpdated: 2021,
+  }),
   comptiaAPlusCore2: new Course({
     courseUrls: [
       {
@@ -12,9 +32,10 @@ export const courses = {
       },
     ],
     duration: 14.82,
+    instructors: [persons.mikeMeyers],
     rating: 5,
     recommendationOrder: 3,
-    title: ' CompTIA A+ Core 2',
+    title: 'CompTIA A+ Core 2',
     yearUpdated: 2021,
   }),
 };
--- apps/website/db/models/course.ts
+++ apps/website/db/models/course.ts
@@ -1,8 +1,10 @@
+import { Person } from './person';
 import { School } from './school';
 
 interface ICourse {
   courseUrls: [{ school: School; url: URL }];
   duration: number;
+  instructors: Person[];
   rating: 1 | 2 | 3 | 4 | 5;
   ratingUrl?: URL;
   recommendationOrder: number;
@@ -13,6 +15,7 @@ interface ICourse {
 export class Course implements ICourse {
   courseUrls: [{ school: School; url: URL }];
   duration: number;
+  instructors: Person[];
   rating: 1 | 2 | 3 | 4 | 5;
   ratingUrl: URL;
   recommendationOrder: number;
@@ -22,6 +25,7 @@ export class Course implements ICourse {
   constructor(parameters: ICourse) {
     this.courseUrls = parameters.courseUrls;
     this.duration = parameters.duration;
+    this.instructors = parameters.instructors;
     this.rating = parameters.rating;
     this.ratingUrl = parameters.ratingUrl;
     this.recommendationOrder = parameters.recommendationOrder;
