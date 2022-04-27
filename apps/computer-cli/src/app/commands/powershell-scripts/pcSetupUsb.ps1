fdb8e2d1e6f32d1074fd642e603b326f10335d44
ethang <hello@ethang.email>
2022-04-26 20:01:26 -0500

    remove old projects

--- apps/computer-cli/src/app/commands/powershell-scripts/pcSetupUsb.ps1
+++
@@ -1,5 +0,0 @@
-# REQUIRES on same USB & file location.
-# chocolatey.license.xml
-# Download and run pcSetup script from https://github.com/eglove/PSScripts/blob/master/pcSetup.ps1
-Start-BitsTransfer -Source https://raw.githubusercontent.com/eglove/PSScripts/master/pcSetup.ps1 -Destination script.ps1
-Invoke-Expression '.\script.ps1'
