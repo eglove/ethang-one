7ae674960062d94bca4a75620f67ab4355e9df3a
ethang <hello@ethang.email>
2022-04-30 11:32:43 -0500

    sonar fixes

--- libs/util-typescript/src/lib/util-number.ts
+++ libs/util-typescript/src/lib/util-number.ts
@@ -1,4 +1,8 @@
 export const randomNumberClientSide = (): number => {
+  if (typeof window === 'undefined') {
+    return Math.random();
+  }
+
   const array = new Uint32Array(1);
   return globalThis.crypto.getRandomValues(array)[0];
 };
