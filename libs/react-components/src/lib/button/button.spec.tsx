8b0bcdfec02a415e00267baf2dc1160f1bf68968
ethang <hello@ethang.email>
2022-05-07 18:15:08 -0500

    button component

--- libs/react-components/src/lib/button/button.spec.tsx
+++ libs/react-components/src/lib/button/button.spec.tsx
@@ -1,10 +1,10 @@
 import { render } from '@testing-library/react';
 
-import Button from './button';
+import { Button } from './button';
 
 describe('Button', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Button />);
+    const { baseElement } = render(<Button size="small" text="Button" />);
     expect(baseElement).toBeTruthy();
   });
 });
