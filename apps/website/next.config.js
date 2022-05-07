d69f3b984ddb7a1bd4b5dbe982bb063b6897960c
ethang <hello@ethang.email>
2022-05-07 18:29:14 -0500

    config

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -1,11 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
+const path = require('path');
 
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
+  experimental: {
+    outputStandalone: true,
+    outputFileTracingRoot: path.join(__dirname, '../../')
+  },
   images: {
     dangerouslyAllowSVG: true,
     domains: ['cdn.filestackcontent.com', 'disquscdn.com', 'rlcdn.com', 'res.cloudinary.com'],
