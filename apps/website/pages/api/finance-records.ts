8637f5c177059e977bedc19cf3b811b9e2b45d89
ethang <hello@ethang.email>
2022-05-07 20:43:05 -0500

    temp remove problem page

--- apps/website/pages/api/finance-records.ts
+++ apps/website/pages/api/finance-records.ts
@@ -1,25 +1,22 @@
-import { FinanceRecord } from '@ethang/ethang-db';
-import { FinanceRecord as FinanceRecordObject } from '@prisma/client';
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default async function (
-  request: NextApiRequest,
-  response: NextApiResponse
-): Promise<FinanceRecordObject[]> {
-  const financeRecord = new FinanceRecord();
-
-  if (request.method === 'POST' && Array.isArray(request.body)) {
-    response
-      .status(200)
-      .json(financeRecord.createMany(request.body as FinanceRecordObject[]));
-    response.end();
-    return;
-  }
-
-  if (request.method === 'GET') {
-    response
-      .status(200)
-      .json(await financeRecord.getFinanceRecordsInLastYear());
-    response.end();
-  }
-}
+export default {};
+// export default async function (
+//   request: NextApiRequest,
+//   response: NextApiResponse
+// ): Promise<FinanceRecordObject[]> {
+//   const financeRecord = new FinanceRecord();
+//
+//   if (request.method === 'POST' && Array.isArray(request.body)) {
+//     response
+//       .status(200)
+//       .json(financeRecord.createMany(request.body as FinanceRecordObject[]));
+//     response.end();
+//     return;
+//   }
+//
+//   if (request.method === 'GET') {
+//     response
+//       .status(200)
+//       .json(await financeRecord.getFinanceRecordsInLastYear());
+//     response.end();
+//   }
+// }
