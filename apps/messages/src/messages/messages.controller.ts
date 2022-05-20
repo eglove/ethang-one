77c7f6127ecc75404c416ffccd396b886124776e
ethang <hello@ethang.email>
2022-05-20 12:02:49 -0500

    messages

--- apps/messages/src/messages/messages.controller.ts
+++ apps/messages/src/messages/messages.controller.ts
@@ -1,16 +1,38 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 import { CreateMessageDto } from './data-transfer-objects/create-message.dto';
+import { Message, Messages } from './messages.repository';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
+  @Get()
+  async listMessages(): Promise<Messages> {
+    return this.messagesService.findAll();
+  }
+
   @Post()
-  createMessage(@Body() body: CreateMessageDto): void {
-    console.info(body);
+  async createMessage(@Body() body: CreateMessageDto): Promise<Message> {
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string): void {
-    console.info(id);
+  async getMessage(@Param('id') id: string): Promise<Message | undefined> {
+    const message = await this.messagesService.findOne(id);
+
+    if (typeof message === 'undefined') {
+      throw new NotFoundException(`Message ${id} not found.`);
+    }
+
+    return message;
   }
 }
