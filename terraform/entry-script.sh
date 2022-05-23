345bdbd2ffc7c2413b761ccaa64f5562a2a370bd
ethang <hello@ethang.email>
2022-05-23 12:52:42 -0500

    changes types

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -3,8 +3,10 @@ sudo yum -y update && sudo yum -y install git
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 16.15.0
-npm i -g pm2
+npm i -g pm2 nx pnpm
 
 git clone https://github.com/eglove/ethang-one.git
-npx --yes nx build graphql-gateway
+cd ethang-one || exit
+pnpm i
+pnpm nx build graphql-gateway
 pm2 start
