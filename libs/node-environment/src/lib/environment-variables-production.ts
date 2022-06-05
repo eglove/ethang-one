ccf748b1f77b52210220fa4231e8438b75c388ee
ethang <hello@ethang.email>
2022-06-04 20:46:40 -0500

    oh yeah...

---
+++ libs/node-environment/src/lib/environment-variables-production.ts
@@ -0,0 +1,10 @@
+import { ENV_KEYS } from './constants';
+import { environmentVariablesDevelopment } from './environment-variables-development';
+
+export const environmentVariablesProduction: Record<
+  ENV_KEYS,
+  string | undefined
+> = {
+  ...environmentVariablesDevelopment,
+  MODE: 'production',
+};
