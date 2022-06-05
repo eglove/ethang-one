84eab08c63532657980ff5f5ec55eff26de285c6
ethang <hello@ethang.email>
2022-06-04 20:06:03 -0500

    move environment variables

--- libs/node-environment/src/lib/environment-variables-production.ts
+++
@@ -1,9 +0,0 @@
-import { ENV_KEYS } from './constants';
-import { environmentVariablesDevelopment } from './environment-variables-development';
-
-export const environmentVariablesProduction: Record<
-  ENV_KEYS,
-  string | undefined
-> = {
-  ...environmentVariablesDevelopment,
-};
