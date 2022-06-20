ab794a0080cbe8d270be9648abed4d93e79c06aa
ethang <hello@ethang.email>
2022-06-20 17:17:33 -0500

    use never for instantiating generics you don't need to know

--- apps/graphql-gateway/src/app/util/interfaces.ts
+++ apps/graphql-gateway/src/app/util/interfaces.ts
@@ -1,23 +1,23 @@
 import { Prisma } from '@prisma/client';
 
 export interface PrismaCrud<ModelType, SelectType> {
-  create: (data: any, select?: SelectType) => Promise<Partial<ModelType>>;
-  createMany: (data: any) => Promise<Prisma.BatchPayload>;
-  delete: (data: any, select?: SelectType) => Promise<Partial<ModelType>>;
-  deleteMany: (data: any) => Promise<Prisma.BatchPayload>;
+  create: (data: never, select?: SelectType) => Promise<Partial<ModelType>>;
+  createMany: (data: never) => Promise<Prisma.BatchPayload>;
+  delete: (data: never, select?: SelectType) => Promise<Partial<ModelType>>;
+  deleteMany: (data: never) => Promise<Prisma.BatchPayload>;
   findFirst: (
-    data: any,
+    data: never,
     select?: SelectType
   ) => Promise<Partial<ModelType> | undefined>;
   findMany: (
-    data: any,
+    data: never,
     select?: SelectType
   ) => Promise<Array<Partial<ModelType>>>;
   findUnique: (
-    data: any,
+    data: never,
     select?: SelectType
   ) => Promise<Partial<ModelType | undefined>>;
-  update: (data: any, select?: SelectType) => Promise<Partial<ModelType>>;
-  updateMany: (data: any) => Promise<Prisma.BatchPayload>;
-  upsert: (data: any, select?: SelectType) => Promise<Partial<ModelType>>;
+  update: (data: never, select?: SelectType) => Promise<Partial<ModelType>>;
+  updateMany: (data: never) => Promise<Prisma.BatchPayload>;
+  upsert: (data: never, select?: SelectType) => Promise<Partial<ModelType>>;
 }
