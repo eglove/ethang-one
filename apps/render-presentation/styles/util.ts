a45b9975dabcd37ce61eb014fa875e44111927ce
ethang <hello@ethang.email>
2022-06-22 10:14:54 -0500

    remove render patterns presentation

--- apps/render-presentation/styles/util.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties } from 'react';
-
-export const twoColumnGrid = (templateColumns: string): CSSProperties => {
-  return {
-    display: 'grid',
-    gridTemplateColumns: templateColumns,
-    placeItems: 'center',
-  };
-};
