65a254f3e92856e536cc6a37f9169a5ba8e45051
ethang <hello@ethang.email>
2022-07-20 20:28:14 -0500

    script test

--- libs/powershell-scripts/git-all.ps1
+++ libs/powershell-scripts/git-all.ps1
@@ -4,15 +4,38 @@ Set-Location ~/Projects/ethang-one
 git add .
 pnpm browserslist --update-db
 npx nx affected --target=lint --fix --base=origin/master --head=HEAD
-$?
-git commit -m "$commitMessage"
-npx nx affected --target=build --base=origin/master --head=HEAD
-$?
-npx nx affected --target=test --base=origin/master --head=HEAD
-$?
-npx nx affected --target=e2e --base=origin/master --head=HEAD
-$?
-npx --yes snyk test
-$?
-Write-Host 'done'
+
+if ($?)
+{
+  git commit -m "$commitMessage"
+  npx nx affected --target=build --base=origin/master --head=HEAD
+} else {
+  git reset
+  Stop-Process
+}
+
+if ($?)
+{
+  npx nx affected --target=test --base=origin/master --head=HEAD
+} else {
+  git reset
+  Stop-Process
+}
+
+if ($?)
+{
+  npx nx affected --target=e2e --base=origin/master --head=HEAD
+} else {
+  git reset
+  Stop-Process
+}
+
+if ($?)
+{
+  npx --yes snyk test
+} else {
+  git reset
+  Stop-Process
+}
+
 #git push
