bec3e435b1a99e0308115f60b7c8b53cbeae319f
ethang <hello@ethang.email>
2022-07-20 20:31:33 -0500

    script test

--- libs/powershell-scripts/git-all.ps1
+++ libs/powershell-scripts/git-all.ps1
@@ -5,33 +5,33 @@ git add .
 pnpm browserslist --update-db
 npx nx affected --target=lint --fix --base=origin/master --head=HEAD
 
-if ($?)
-{
+if ($?) {
   git commit -m "$commitMessage"
   npx nx affected --target=build --base=origin/master --head=HEAD
 } else {
   Break
 }
 
-if ($?)
-{
+if ($?) {
   npx nx affected --target=test --base=origin/master --head=HEAD
 } else {
   Break
 }
 
-if ($?)
-{
+if ($?) {
   npx nx affected --target=e2e --base=origin/master --head=HEAD
 } else {
   Break
 }
 
-if ($?)
-{
+if ($?) {
   npx --yes snyk test
 } else {
   Break
 }
 
-git push
+if ($?) {
+  git push
+} else {
+  Break
+}
